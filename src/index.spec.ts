00b5bada5d563c885c3f41056abfbc7a8fd3c2d8
willclarktech <willclarktech@users.noreply.github.com>
2020-06-29 19:01:40 +0200

    Add basic linear regression example

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,10 +1,12 @@
-import { createNetwork } from "./";
+import { createNetwork, prepareData, train } from "./";
+import carsData from "./data/cars.json";
 
-describe("whatever", () => {
-	it("tests", () => {
+describe("basic network", () => {
+	it("trains", async () => {
 		const model = createNetwork();
-		console.log(model);
-
-		expect(false).toBe(true);
+		const { inputs, labels } = prepareData(carsData);
+		const history = await train(model, inputs, labels);
+		const losses = history.history.loss;
+		return expect(losses[losses.length - 1]).toBeLessThan(0.05);
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,59 @@
 import * as tf from "@tensorflow/tfjs-node";
 
+import carsData from "./data/cars.json";
+
+type PreparedData = {
+	readonly inputs: tf.Tensor;
+	readonly labels: tf.Tensor;
+	readonly inputMax: tf.Tensor;
+	readonly inputMin: tf.Tensor;
+	readonly labelMax: tf.Tensor;
+	readonly labelMin: tf.Tensor;
+};
+
+type NormalizationParameters = {
+	readonly inputMax: tf.Tensor;
+	readonly inputMin: tf.Tensor;
+	readonly labelMax: tf.Tensor;
+	readonly labelMin: tf.Tensor;
+};
+
+export const prepareData = (
+	data: readonly { readonly mpg: number; readonly horsepower: number }[],
+): PreparedData => {
+	const mutableData = Array.from(data);
+	return tf.tidy(() => {
+		tf.util.shuffle(mutableData);
+
+		const inputs = mutableData.map((datum) => datum.horsepower);
+		const labels = mutableData.map((datum) => datum.mpg);
+
+		const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+		const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+		const inputMax = inputTensor.max();
+		const inputMin = inputTensor.min();
+		const labelMax = labelTensor.max();
+		const labelMin = labelTensor.min();
+
+		const normalizedInputs = inputTensor
+			.sub(inputMin)
+			.div(inputMax.sub(inputMin));
+		const normalizedLabels = labelTensor
+			.sub(labelMin)
+			.div(labelMax.sub(labelMin));
+
+		return {
+			inputs: normalizedInputs,
+			labels: normalizedLabels,
+			inputMax,
+			inputMin,
+			labelMax,
+			labelMin,
+		};
+	});
+};
+
 export const createNetwork = (): tf.Sequential => {
 	const network = tf.sequential();
 	network.add(
@@ -15,3 +69,59 @@ export const createNetwork = (): tf.Sequential => {
 	);
 	return network;
 };
+
+export const train = async (
+	model: tf.Sequential,
+	inputs: tf.Tensor,
+	labels: tf.Tensor,
+): Promise<tf.History> => {
+	const compileArgs = {
+		optimizer: tf.train.adam(),
+		loss: tf.losses.meanSquaredError,
+	};
+	model.compile(compileArgs);
+
+	const fitArgs = {
+		batchSize: 32,
+		epochs: 50,
+		shuffle: true,
+	};
+	return model.fit(inputs, labels, fitArgs);
+};
+
+export const testModel = (
+	model: tf.Sequential,
+	{ inputMax, inputMin, labelMax, labelMin }: NormalizationParameters,
+): readonly { readonly x: number; readonly y: number }[] => {
+	const [xs, predictions] = tf.tidy(() => {
+		const raw_xs = tf.linspace(0, 1, 100);
+		const raw_predictions = model.predict(
+			raw_xs.reshape([100, 1]),
+		) as tf.Tensor;
+
+		const unnormalized_xs = raw_xs
+			.mul(inputMax.sub(inputMin))
+			.add(inputMin)
+			.dataSync();
+		const unnormalized_predictions = raw_predictions
+			.mul(labelMax.sub(labelMin))
+			.add(labelMin)
+			.dataSync();
+
+		return [unnormalized_xs, unnormalized_predictions];
+	});
+
+	return Array.from(xs).map((x, i) => ({ x, y: predictions[i] }));
+};
+
+const main = async (): Promise<void> => {
+	const model = createNetwork();
+	const preparedData = prepareData(carsData);
+	await train(model, preparedData.inputs, preparedData.labels);
+	const results = testModel(model, preparedData);
+	console.info(results);
+};
+
+if (process.env.NODE_ENV !== "TEST") {
+	main();
+}
