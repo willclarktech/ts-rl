9c5334271aa44077b21d07725773e88747089680
willclarktech <willclarktech@users.noreply.github.com>
2020-07-03 23:43:16 +0200

    Improve browser script

--- results/script.js
+++ results/script.js
@@ -1,29 +1,42 @@
 /* globals document,XMLHttpRequest,tfvis */
 
-async function loadJSON(path) {
+const loadJSON = async (path) => {
 	const xobj = new XMLHttpRequest();
 	xobj.overrideMimeType("application/json");
 	xobj.open("GET", path, true);
 	return new Promise((resolve) => {
-		xobj.onreadystatechange = function () {
-			if (xobj.readyState == 4 && xobj.status == "200") {
-				// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+		xobj.onreadystatechange = () => {
+			if (xobj.readyState === 4 && xobj.status === 200) {
 				resolve(JSON.parse(xobj.responseText));
 			}
 		};
 		xobj.send(null);
 	});
-}
+};
 
 async function run() {
-	const data = await loadJSON("data/xxx.json");
+	const dataDir = "data";
+	const experimentName = "cars";
+	const fileName = `${dataDir}/${experimentName}.json`;
+	const {
+		name,
+		xLabel,
+		yLabel,
+		height,
+		originalData,
+		predictionData,
+	} = await loadJSON(fileName);
+
 	tfvis.render.scatterplot(
-		{ name: "Horsepower v MPG" },
-		{ values: data },
+		{ name },
 		{
-			xLabel: "Horsepower",
-			yLabel: "MPG",
-			height: 300,
+			values: [originalData, predictionData],
+			series: ["original data", "prediction data"],
+		},
+		{
+			xLabel,
+			yLabel,
+			height,
 		},
 	);
 }
