97bdee01c62d87d6e96dc157eb0cab568cd77285
willclarktech <willclarktech@users.noreply.github.com>
2020-09-28 17:50:28 +0200

    Add REINFORCE placeholder

--- src/index.ts
+++ src/index.ts
@@ -1,177 +1,74 @@
-import * as tf from "@tensorflow/tfjs-node";
-import fs from "fs";
+import { CartPole } from "./cart-pole";
+import { Environment } from "./core";
+import { ReinforceLearner } from "./reinforce";
+import { logEpisode, mean } from "./util";
 
-import carsData from "./data/cars.json";
+const train = (
+	env: Environment,
+	learner: ReinforceLearner,
+	maxEpisodes: number,
+	rollingAveragePeriod: number,
+	logPeriod: number,
+	logFile: string,
+): boolean => {
+	const returns = [];
+	const rollingAverageReturns = [];
 
-type PreparedData = {
-	readonly inputs: readonly number[];
-	readonly labels: readonly number[];
-	readonly normalizedInputs: tf.Tensor;
-	readonly normalizedLabels: tf.Tensor;
-	readonly inputMax: tf.Tensor;
-	readonly inputMin: tf.Tensor;
-	readonly labelMax: tf.Tensor;
-	readonly labelMin: tf.Tensor;
+	for (let episode = 1; episode <= maxEpisodes; ++episode) {
+		const ret = learner.runEpisode(env);
+		returns.push(ret);
+
+		const rollingAverageReturn = mean(returns.slice(-100));
+		rollingAverageReturns.push(rollingAverageReturn);
+
+		if (episode % logPeriod === 0) {
+			logEpisode(
+				episode,
+				returns,
+				rollingAveragePeriod,
+				rollingAverageReturns,
+				logFile,
+			);
+		}
+
+		if (
+			env.winningScore !== undefined &&
+			episode >= rollingAveragePeriod &&
+			rollingAverageReturn >= env.winningScore
+		) {
+			return true;
+		}
+	}
+
+	return false;
 };
 
-type NormalizationParameters = {
-	readonly inputMax: tf.Tensor;
-	readonly inputMin: tf.Tensor;
-	readonly labelMax: tf.Tensor;
-	readonly labelMin: tf.Tensor;
-};
+const main = (): void => {
+	const env = new CartPole();
 
-type Point = {
-	readonly x: number;
-	readonly y: number;
-};
+	const hiddenWidths = [4, 4];
+	const alpha = 0.001; // Learning rate
+	const gamma = 0.99; // Discount rate
+	const learner = new ReinforceLearner(env, hiddenWidths, alpha, gamma);
 
-const normalizeData = <T extends tf.Tensor>(
-	tensor: T,
-	min = tensor.min(),
-	max = tensor.max(),
-): T => tensor.sub(min).div(max.sub(min));
+	const maxEpisodes = 1000;
+	const rollingAveragePeriod = 100;
+	const logPeriod = 100;
+	const experimentName = `${env.name}-${learner.name}`;
+	const logFile = `./results/data/${experimentName}.json`;
 
-export const prepareData = (
-	data: readonly { readonly mpg: number; readonly horsepower: number }[],
-): PreparedData => {
-	const mutableData = Array.from(data);
-	return tf.tidy(() => {
-		tf.util.shuffle(mutableData);
+	console.info("Score to beat:", env.winningScore);
 
-		const inputs = mutableData.map((datum) => datum.horsepower);
-		const labels = mutableData.map((datum) => datum.mpg);
-
-		const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
-		const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
-
-		const inputMax = inputTensor.max();
-		const inputMin = inputTensor.min();
-		const labelMax = labelTensor.max();
-		const labelMin = labelTensor.min();
-
-		const normalizedInputs = normalizeData(inputTensor, inputMin, inputMax);
-		const normalizedLabels = normalizeData(labelTensor, labelMin, labelMax);
-
-		return {
-			inputs: inputs,
-			labels: labels,
-			normalizedInputs: normalizedInputs,
-			normalizedLabels: normalizedLabels,
-			inputMax,
-			inputMin,
-			labelMax,
-			labelMin,
-		};
-	});
-};
-
-export const createNetwork = (
-	widths: readonly number[],
-	activation: "sigmoid",
-): tf.Sequential => {
-	const network = tf.sequential({
-		name: "tutorial-2d",
-		layers: widths.slice(1).map((width, i) =>
-			i === 0
-				? tf.layers.dense({
-						inputShape: [widths[0]],
-						units: width,
-						activation,
-				  })
-				: i !== widths.length - 2
-				? tf.layers.dense({ units: width, activation })
-				: tf.layers.dense({ units: width }),
-		),
-	});
-	return network;
-};
-
-export const train = async (
-	model: tf.Sequential,
-	inputs: tf.Tensor,
-	labels: tf.Tensor,
-	hyperparameters: tf.ModelFitArgs & { readonly learningRate: number },
-): Promise<tf.History> => {
-	const compileArgs = {
-		optimizer: tf.train.adam(hyperparameters.learningRate),
-		loss: tf.losses.meanSquaredError,
-	};
-	model.compile(compileArgs);
-
-	return model.fit(inputs, labels, hyperparameters);
-};
-
-export const testModel = (
-	model: tf.Sequential,
-	{ inputMax, inputMin, labelMax, labelMin }: NormalizationParameters,
-): readonly tf.TypedArray[] => {
-	return tf.tidy(() => {
-		const rawXs = tf.linspace(0, 1, 100);
-		const rawPredictions = model.predict(rawXs.reshape([100, 1])) as tf.Tensor;
-
-		const unnormalizedXs = rawXs
-			.mul(inputMax.sub(inputMin))
-			.add(inputMin)
-			.dataSync();
-		const unnormalizedPredictions = rawPredictions
-			.mul(labelMax.sub(labelMin))
-			.add(labelMin)
-			.dataSync();
-
-		return [unnormalizedXs, unnormalizedPredictions];
-	});
-};
-
-const createPlotData = (
-	xs: readonly number[],
-	ys: readonly number[],
-): readonly Point[] =>
-	Array.from(xs).map((x: number, i: number): Point => ({ x, y: ys[i] }));
-
-const main = async (): Promise<void> => {
-	const preparedData = prepareData(carsData);
-
-	const activationFunction = "sigmoid";
-	const model = createNetwork([1, 4, 4, 1], activationFunction);
-
-	const hyperparameters = {
-		batchSize: 32,
-		epochs: 20,
-		shuffle: true,
-		learningRate: 0.03,
-	};
-	await train(
-		model,
-		preparedData.normalizedInputs,
-		preparedData.normalizedLabels,
-		hyperparameters,
-	);
-	const predictions = testModel(model, preparedData);
-
-	const dataDir = "./results/data";
-	const experimentName = "cars";
-	const fileName = `${dataDir}/${experimentName}.json`;
-
-	const originalData = createPlotData(preparedData.inputs, preparedData.labels);
-	const predictionData = createPlotData(
-		Array.from(predictions[0]),
-		Array.from(predictions[1]),
+	const didWin = train(
+		env,
+		learner,
+		maxEpisodes,
+		rollingAveragePeriod,
+		logPeriod,
+		logFile,
 	);
 
-	fs.writeFileSync(
-		fileName,
-		JSON.stringify({
-			name: "Horsepower v MPG",
-			xLabel: "Horsepower",
-			yLabel: "MPG",
-			height: 300,
-			originalData,
-			predictionData,
-		}),
-	);
+	console.info(didWin ? "You won!" : "You lost.");
 };
 
-if (process.env.NODE_ENV !== "TEST") {
-	main();
-}
+main();
---
+++ src/reinforce.ts
@@ -0,0 +1,113 @@
+import * as tf from "@tensorflow/tfjs-node";
+
+import { Environment, Learner, Observation } from "./core";
+import { createNetwork, mean, sum } from "./util";
+
+const calculateDiscountedReward = (
+	rewards: readonly number[],
+	gamma: number,
+): number => sum(rewards.map((reward, i) => reward * gamma ** i));
+
+const calculateDiscountedRewards = (
+	rewards: readonly number[],
+	gamma: number,
+): readonly number[] =>
+	rewards.map((_, i) => calculateDiscountedReward(rewards.slice(i), gamma));
+
+const normalizeRewards = (
+	rewards: readonly number[],
+	epsilon = 1e-9,
+): readonly number[] => {
+	const mn = mean(rewards);
+	const std = tf.moments(rewards).variance.sqrt().dataSync()[0];
+	return rewards.map((reward) => reward - mn / (std + epsilon));
+};
+
+const discountAndNormalizeRewards = (
+	rewards: readonly number[],
+	gamma: number,
+): readonly number[] => {
+	const discountedRewards = calculateDiscountedRewards(rewards, gamma);
+	return normalizeRewards(discountedRewards);
+};
+
+type Sample = {
+	readonly observation: Observation;
+	readonly reward: number;
+	readonly done: boolean;
+	readonly logProbability: tf.Tensor1D;
+};
+
+export class ReinforceLearner implements Learner {
+	public name: string;
+
+	private gamma: number;
+	private network: tf.Sequential;
+	private optimizer: tf.Optimizer;
+
+	constructor(
+		{ numObservationDimensions, numActions }: Environment,
+		hiddenWidths: readonly number[],
+		alpha: number,
+		gamma: number,
+	) {
+		this.name = "Reinforce";
+		this.gamma = gamma;
+		this.optimizer = tf.train.adam(alpha);
+		const widths = [numObservationDimensions, ...hiddenWidths, numActions];
+		this.network = createNetwork(widths, "relu", "softmax");
+	}
+
+	private getSample(env: Environment, observation: Observation): Sample {
+		const processedObservation = tf
+			.tensor1d([...observation])
+			.reshape<tf.Tensor2D>([1, 4]);
+		const output = this.network.predict(processedObservation) as tf.Tensor2D;
+		const squeezedOutput = output.squeeze<tf.Tensor1D>([0]);
+		const action = tf.multinomial(squeezedOutput, 1).dataSync()[0];
+		const logProbability = tf.log(squeezedOutput.gather([action]));
+		const { observation: nextObservation, reward, done } = env.step(action);
+
+		return {
+			observation: nextObservation,
+			reward,
+			done,
+			logProbability: logProbability,
+		};
+	}
+
+	private calculateLoss(
+		rewards: readonly number[],
+		logProbabilities: tf.Tensor1D,
+	): tf.Scalar {
+		const normalizedDiscountedRewards = discountAndNormalizeRewards(
+			rewards,
+			this.gamma,
+		);
+		return tf.sum(logProbabilities.mul(normalizedDiscountedRewards)).mul(-1);
+	}
+
+	public runEpisode(env: Environment): number {
+		let observation = env.reset();
+		let done = false;
+		let rewards: readonly number[] = [];
+		let logProbabilities = tf.tensor1d([]);
+
+		tf.tidy(() => {
+			this.optimizer.minimize(() => {
+				while (!done) {
+					const sample = this.getSample(env, observation);
+					({ observation, done } = sample);
+					const { reward, logProbability } = sample;
+
+					rewards = [...rewards, reward];
+					logProbabilities = logProbabilities.concat(logProbability);
+				}
+
+				return this.calculateLoss(rewards, logProbabilities);
+			});
+		});
+
+		return sum(rewards);
+	}
+}
