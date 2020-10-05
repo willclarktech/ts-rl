b487fb74c3f06eb750323c2750dd468d1a18642a
willclarktech <willclarktech@users.noreply.github.com>
2020-10-05 17:47:28 +0200

    Add BalancedReplayMemory class

--- src/replay-memory.ts
+++ src/replay-memory.ts
@@ -9,8 +9,14 @@ export interface Transition {
 	readonly nextObservation: Observation;
 }
 
-export class ReplayMemory {
-	private capacity: number;
+export interface ReplayMemory {
+	readonly size: number;
+	readonly store: (transition: Transition) => void;
+	readonly sample: (n: number) => readonly Transition[];
+}
+
+export class BasicReplayMemory implements ReplayMemory {
+	private readonly capacity: number;
 	private transitions: readonly Transition[];
 
 	public constructor(capacity: number) {
@@ -46,6 +52,64 @@ export class ReplayMemory {
 				};
 			},
 			{ sample: initialSample, transitions: this.transitions },
+		).sample;
+	}
+}
+
+export class BalancedReplayMemory implements ReplayMemory {
+	private readonly positiveCapacity: number;
+	private readonly negativeCapacity: number;
+	private positiveTransitions: readonly Transition[];
+	private negativeTransitions: readonly Transition[];
+
+	public constructor(positiveCapacity: number, negativeCapacity: number) {
+		this.positiveCapacity = positiveCapacity;
+		this.negativeCapacity = negativeCapacity;
+		this.positiveTransitions = [];
+		this.negativeTransitions = [];
+	}
+
+	public get size(): number {
+		return this.positiveTransitions.length + this.negativeTransitions.length;
+	}
+
+	public store(transition: Transition): void {
+		if (transition.reward >= 0) {
+			this.positiveTransitions = [
+				...this.positiveTransitions,
+				transition,
+			].slice(-this.positiveCapacity);
+		} else {
+			this.negativeTransitions = [
+				...this.negativeTransitions,
+				transition,
+			].slice(-this.negativeCapacity);
+		}
+	}
+
+	public sample(n: number): readonly Transition[] {
+		const initialSample: readonly Transition[] = [];
+		return Array.from({ length: n }).reduce(
+			({
+				sample,
+				transitions,
+			}): {
+				readonly sample: readonly Transition[];
+				readonly transitions: readonly Transition[];
+			} => {
+				const i = sampleUniform(transitions.length);
+				return {
+					sample: [...sample, transitions[i]],
+					transitions: [
+						...transitions.slice(0, i),
+						...transitions.slice(i + 1),
+					],
+				};
+			},
+			{
+				sample: initialSample,
+				transitions: [...this.positiveTransitions, ...this.negativeTransitions],
+			},
 		).sample;
 	}
 }
